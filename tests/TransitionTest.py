0a38b0141cc04cc48802999f4db11994d4e1384c
Ian Dunn <techwizguy7@gmail.com>
2024-02-14 13:10:10 -0500

    Transition tests complete

--- tests/TransitionTest.py
+++ tests/TransitionTest.py
@@ -1,8 +1,6 @@
-import os
 import tkinter as tk
 
-from playsound import playsound
-
+import config
 from LSL import LSL
 from tests.TestGUI import TestGUI
 from tests.TestThread import TestThread
@@ -10,48 +8,56 @@ from tests.TestThread import TestThread
 
 class TransitionTest(TestThread):
     """
-    TODO
+
     """
 
     def __init__(self, name, image_directory_1, image_directory_2):
         """
         Initializes and creates the transition labels in the display window.
         """
-        super().__init__(name, 15*1000)
+        super().__init__(name)
 
         self.image_1 = tk.PhotoImage(file=image_directory_1)
         self.image_2 = tk.PhotoImage(file=image_directory_2)
 
-        # TODO fix labels
-        self.label_1 = name.split("To")[0]
-        self.label_2 = name.split("To")[1]
+        split = " to " if " to " in name else " and "
+        self.label_1 = name.split(split)[0]
+        self.label_2 = name.split(split)[1]
 
         self.firstImage = True
         self.current_label = None
 
-    def start_iteration(self):
+    def run_test(self):
         """
-        Creates and displays a new label for each iteration.
+        Main loop that runs and schedules the next iteration of the test
         """
-        super().start_iteration()
+        if self.iteration == config.ITERATIONS_PER_ACTION:
+            self.running = False
 
-        if self.firstImage:
-            LSL.start_label(self.label_1)
-            self.current_label = tk.Label(TestGUI.display_window, image=self.image_1, borderwidth=0)
+        if self.running:
+            if self.firstImage:
+                LSL.start_label(self.label_1)
+                self.current_label = tk.Label(TestGUI.display_window, image=self.image_1, borderwidth=0)
+            else:
+                LSL.start_label(self.label_2)
+                self.current_label = tk.Label(TestGUI.display_window, image=self.image_2, borderwidth=0)
+
+            self.playsound()
+            self.current_label.place(relx=0.5, rely=0.5, anchor='center')
+
+            def swap():
+                self.firstImage = not self.firstImage
+                self.current_label.destroy()
+                self.run_test()
+
+            TestGUI.display_window.after(10000, swap)  # TODO how long per transition state + config
+
+            self.iteration += 1
         else:
-            LSL.start_label(self.label_2)
-            self.current_label = tk.Label(TestGUI.display_window, image=self.image_2, borderwidth=0)
+            # Stop test thread
+            self.running = False
+            self.current_label.destroy()
 
-        self.firstImage = not self.firstImage
-        self.playsound()
-        LSL.start_label(self.name)
-        self.current_label.place(relx=0.5, rely=0.5, anchor='center')
-
-    def stop_iteration(self):
-        """
-        Destroys the label after each iteration.
-        """
-        super().stop_iteration()
-
-        self.current_label.destroy()
-
+            LSL.stop_label()
+            self.stop()
+            TestGUI.display_window.after(1, self.stop)
