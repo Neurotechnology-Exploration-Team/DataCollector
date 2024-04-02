14f9ba00e72c720647b7729ec07412b66a1af5c4
Alexandr5656 <alexburbano5656@gmail.com>
2024-04-02 15:50:25 -0400

    Working with config

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ DATA_PADDING_DURATION = 5    # How long to wait before starting and ending a tes
 BLINK_MIN_INTERVAL = 2.5     # The minimum duration between intervals (default 2.5 seconds)
 BLINK_MAX_INTERVAL = 6       # The maximum duration between intervals (default 6 seconds)
 
-ITERATIONS_PER_TEST = 30           # How many iterations run per action (default 30)
-ITERATIONS_PER_CONSTANT_TEST = 10  # How many iterations run per constant test (default 5, generally about 5 minutes)
+ITERATIONS_PER_TEST = 15           # How many iterations run per action (default 30)
+ITERATIONS_PER_CONSTANT_TEST = 7  # How many iterations run per constant test (default 5, generally about 5 minutes)
 
 PAUSE_AFTER_TEST = 0.5       # How long to wait after each test iteration (default 0.5 seconds)
 TRANSITION_DURATION = 10     # How long to wait before swapping transition states (default 10 seconds)
-CONSTANT_TEST_DURATION = 20  # How long a constant test iteration should be (default 20 seconds)
+CONSTANT_TEST_DURATION = 15  # How long a constant test iteration should be (default 20 seconds)
 CONSTANT_TEST_BREAK = 5      # How long to break for during constant tests (default 5 seconds)
 
 #
@@ -28,8 +28,8 @@ TESTS = {
         "Stationary Float to Select": ["Stop.png", "Select.png"],
         "Stationary Float to Float Left": ["Stop.png", "Left.png"],
         "Stationary Float to Float Right": ["Stop.png", "Right.png"],
-        "Stationary Float to Float Up": ["Stop.png", "Up.png"],
-        "Stationary Float to Float Down": ["Stop.png", "Down.png"],
+        "Stationary Float to Float Forward": ["Stop.png", "Up.png"],
+        "Stationary Float to Float Backward": ["Stop.png", "Down.png"],
         "Eyes Open to Eyes Closed": ["EyesOpen.png", "EyesClosed.png"],
         "Brow Furrow to Brow Unfurrow": ["BrowFurrow.png", "BrowUnfurrow.png"]
     },
@@ -41,8 +41,8 @@ TESTS = {
         "Stationary Floating": "Stop.png",
         "Float Left": "Left.png",
         "Float Right": "Right.png",
-        "Float Up": "Up.png",
-        "Float Down": "Down.png",
+        "Float Forward": "Up.png",
+        "Float Backward": "Down.png",
         "Select": "Select.png"
     },
     "Blink": [  # No images for blinking tests
@@ -51,7 +51,7 @@ TESTS = {
 }
 
 # Subject information
-NUMBER_OF_SUBJECTS = 10
+NUMBER_OF_SUBJECTS = 10 #Is there a limit to amount of subjects?
 
 # Save path configuration (<DATA_PATH>/PXXX/SXXX/trial_XX/<STREAM_TYPE>_data.csv)
 SAVED_DATA_PATH = "csv_downloads"
