2c6dd931408c645884c71de10672c9ee7f99828c
Aidan Fogg Anderson <thatonecoldguy@gmail.com>
2025-03-24 20:44:42 -0400

    Adds blink_image graphic

--- tests/BlinkTest.py
+++ tests/BlinkTest.py
@@ -2,6 +2,7 @@ import random
 from time import sleep
 
 import config
+import tkinter as tk
 from LSL import LSL
 from tests.TestGUI import TestGUI
 from tests.TestThread import TestThread
@@ -16,6 +17,8 @@ class BlinkTest(TestThread):
 
     def __init__(self, name):
         super().__init__(name)
+        self.blink_image = tk.PhotoImage(file="assets/Blink.png")
+        self.current_image = None
 
     def run_test(self):
         """
@@ -27,6 +30,7 @@ class BlinkTest(TestThread):
         if self.running:
             # Blink & setup next interval
             LSL.start_label(self.name)
+            self.current_image = TestGUI.place_image(self.blink_image)
             self.playsound()
             self.iteration += 1
 
